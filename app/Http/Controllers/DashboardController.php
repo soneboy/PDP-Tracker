eb76846bf298933df6dc4545f8c5c192de35b56e
Nebojsa Masic <nebojsa.masic@devtechgroup.com>
2018-03-07 17:39:47 +0100

    First commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php namespace App\Http\Controllers;
+
+class DashboardController extends Controller {
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function indexAction()
+	{
+		return view('pages.dashboard')->with('page', 'Dashboard');
+	}
+}
